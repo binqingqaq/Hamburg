4c76e4a6f357e830e67884dfd1c226d5b028bb50
Binqing <3519958191@qq.com>
2022-04-29 21:54:12 +0800

    1.0；设计了管理系统总界面

---
+++ src/main/java/com/lanqiao/hamburg/Form/Main.java
@@ -0,0 +1,102 @@
+/*
+ * Created by JFormDesigner on Fri Apr 29 21:53:01 CST 2022
+ */
+
+package com.lanqiao.hamburg.Form;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @author 1
+ */
+public class Main extends JFrame {
+    public Main() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        tabbedPane1 = new JTabbedPane();
+        panel1 = new JPanel();
+        panel2 = new JPanel();
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(null);
+
+        //======== tabbedPane1 ========
+        {
+            tabbedPane1.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+            tabbedPane1.setTabPlacement(SwingConstants.LEFT);
+
+            //======== panel1 ========
+            {
+                panel1.setLayout(null);
+
+                {
+                    // compute preferred size
+                    Dimension preferredSize = new Dimension();
+                    for(int i = 0; i < panel1.getComponentCount(); i++) {
+                        Rectangle bounds = panel1.getComponent(i).getBounds();
+                        preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
+                        preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
+                    }
+                    Insets insets = panel1.getInsets();
+                    preferredSize.width += insets.right;
+                    preferredSize.height += insets.bottom;
+                    panel1.setMinimumSize(preferredSize);
+                    panel1.setPreferredSize(preferredSize);
+                }
+            }
+            tabbedPane1.addTab("1", panel1);
+
+            //======== panel2 ========
+            {
+                panel2.setLayout(null);
+
+                {
+                    // compute preferred size
+                    Dimension preferredSize = new Dimension();
+                    for(int i = 0; i < panel2.getComponentCount(); i++) {
+                        Rectangle bounds = panel2.getComponent(i).getBounds();
+                        preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
+                        preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
+                    }
+                    Insets insets = panel2.getInsets();
+                    preferredSize.width += insets.right;
+                    preferredSize.height += insets.bottom;
+                    panel2.setMinimumSize(preferredSize);
+                    panel2.setPreferredSize(preferredSize);
+                }
+            }
+            tabbedPane1.addTab("2", panel2);
+        }
+        contentPane.add(tabbedPane1);
+        tabbedPane1.setBounds(0, 0, 700, 500);
+
+        {
+            // compute preferred size
+            Dimension preferredSize = new Dimension();
+            for(int i = 0; i < contentPane.getComponentCount(); i++) {
+                Rectangle bounds = contentPane.getComponent(i).getBounds();
+                preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
+                preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
+            }
+            Insets insets = contentPane.getInsets();
+            preferredSize.width += insets.right;
+            preferredSize.height += insets.bottom;
+            contentPane.setMinimumSize(preferredSize);
+            contentPane.setPreferredSize(preferredSize);
+        }
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JPanel panel2;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
