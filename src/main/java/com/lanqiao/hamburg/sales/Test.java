a83a857c47b8234a09111f8b3b3d7894554de0a6
lcx <2522387893@qq.com>
2022-05-06 23:47:18 +0800

    实现了排序功能

---
+++ src/main/java/com/lanqiao/hamburg/sales/Test.java
@@ -0,0 +1,14 @@
+package com.lanqiao.hamburg.sales;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * @Author Lenovo
+ * @Date 2022/5/6 23:46
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("aohid");
+    }
+}
