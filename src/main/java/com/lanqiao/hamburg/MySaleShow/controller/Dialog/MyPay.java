b5a376d1980ee9db86f7ed2f6fae96ec7eb4eb2a
qin <2926594808@qq.com>
2022-05-11 11:22:16 +0800

    优化V1.6,新增测试微信支付功能，通过maven项目集成实现，未优化接口，未优化订单与支付关系，每次生成只需支付0.01，下次优化为两天后。

--- src/main/java/com/lanqiao/hamburg/MySaleShow/controller/Dialog/MyPay.java
+++ src/main/java/com/lanqiao/hamburg/MySaleShow/controller/Dialog/MyPay.java
@@ -5,6 +5,7 @@
 package com.lanqiao.hamburg.MySaleShow.controller.Dialog;
 
 
+import cn.juntai.wxpaydemo.pay.WXPay;
 import com.lanqiao.hamburg.MySaleShow.entity.ShopCar;
 
 
@@ -70,8 +71,8 @@ public class MyPay extends JDialog {
         button1.setBounds(new Rectangle(new Point(110, 145), button1.getPreferredSize()));
         button1.addActionListener(a->{
             try {
-                //new WXPay().UsePay();
-                ImageIcon imageIcon  = new ImageIcon("src/main/java/ImageDemo/new.jpg");
+                new WXPay().UsePay();
+                ImageIcon imageIcon  = new ImageIcon("src/main/java/com/lanqiao/hamburg/image/dishes/new.jpg");
                 imageIcon.setImage(imageIcon.getImage().getScaledInstance(120,120,Image.SCALE_DEFAULT));
                 JLabel Ma = new JLabel(imageIcon);
                 this.add(Ma);
